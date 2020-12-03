0048c86da1e12676fe33b0660a87cd7c4d3c0334
BarakFalach <barak8995@gmail.com>
2020-12-04 00:03:57 +0200

    changes to the trush

--- server.js
+++ server.js
@@ -2,9 +2,15 @@ const express = require('express');
 const connectDB = require('./config/db');
 
 const app = express();
+//  Connect Datebase
 connectDB();
 
-app.get('/',(req, res) => res.send('API Running'));
+app.get('/',(req, res) => res.send('it is working!'));
+
+app.use('./api/users' , require('./routes/api/users'));
+app.use('./api/auth' , require('./routes/api/auth'));
+app.use('./api/posts' , require('./routes/api/posts'));
+app.use('./api/profile' , require('./routes/api/profile'));
 
 const PORT = process.env.PORT || 5000;
 
