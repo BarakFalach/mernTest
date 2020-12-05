a6c05f772705903aafd7d07a64051deae3c4b5a6
BarakFalach <barak8995@gmail.com>
2020-12-05 14:12:55 +0200

    Lesson 10

--- server.js
+++ server.js
@@ -5,10 +5,13 @@ const app = express();
 //  Connect Database
 connectDB();
 
+//init Middleware
+app.use(express.json({extended:false}));
+
 app.get('/',(req, res) => res.send('it is working!'));
 
 
-// Define Routes
+// Define Routesnpm 
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/posts', require('./routes/api/posts'));
