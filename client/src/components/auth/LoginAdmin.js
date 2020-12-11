23df49ec8c7cde8592257d0977b8d4bf95418a63
berdugo1994 <berdugogo@gmail.com>
2020-12-11 14:33:21 +0200

    dg

--- client/src/components/auth/LoginAdmin.js
+++ client/src/components/auth/LoginAdmin.js
@@ -1,119 +1,91 @@
 import React, {Fragment, useState} from 'react'
-import { Link , Redirect} from 'react-router-dom'
-import axios from 'axios';
 import DribbleButton from 'react-dribble-button';
-import styles from './EdenCss.css';
 
 
 
 const LoginAdmin = () => {
-    
-
 
     const [formData, setDataForm] = useState({
         email: "",
         password: ""
-    })
-    
+    }) 
 
     const {email, password} = formData;
-    
 
     const onChange = e => setDataForm({...formData, [e.target.name]: e.target.value});
-    const Login = async e => {
-        e.preventDefault(); 
-        var res ;
-
-        try {
-            const config = {
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            }
-        
-            const body = JSON.stringify(formData);    
-            res = await axios.post('/api/auth', body, config);
-            console.log(res.data.token);
-
-        } catch (err){
-            console.log("Error falah");
-            throw err;
-        }
-
-        try {
-            const config = {
-                headers: {
-                    'x-auth-token': res.data.token
-                }
-            }
-            const tempAdmin = await axios.get('/api/auth', config);
-
-            return <Redirect 
-                        to= {{
-                            pathname: '/admin' ,
-                            state: {admin: tempAdmin}
-                            }}></Redirect>
-        } catch (err){
-            console.log("Error Asi");
-            throw err;
-        }
+    const onLogin = e => {
+        e.preventDefault();  
     }
 
     
     return (
+        <Fragment>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+            <link
+            href="https://fonts.googleapis.com/css?family=Raleway"
+            rel="stylesheet"
+            />
+            <link
+            rel="stylesheet"
+            href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
+            integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf"
+            crossorigin="anonymous"
+            />
 
-        // <Fragment>
-            
-        
+            <link rel="stylesheet" href="css/style.css" />
+            <title>Welcome To The Developer Connector</title>
+        </head>
         <body>
-          {/* <section className="container"> */}
-            <h1 className="large text-primary">
+            <section class="container">
+            <h1 class="large text-primary" class="col-centered">
                 Sign In
             </h1>
-            <div className="form-group">
-              <input 
-                type="text"
-                // barakfalah8995@gmail.com
-                placeholder="Email Address"
-                name="email" 
-                value={email}
-                onChange = {e => onChange(e)}
-                required/>
-            </div>
-            <div className="form-group">
-              <input 
-                type="password"
-                placeholder="Password"
-                name="password"
-                value={password}
-                onChange = {e => onChange(e)} 
-                required />
-            </div>
-            <div>
-              <button 
-                type="button" 
-                className="btn sign_in"
-                onClick=  {e => Login(e)}>
-                sign in
-              </button>       
-            </div>
-            <div className="message create account">
-              <h5>
-                  Don't have an account yet?
-              </h5>
-            </div>
-            <div className="sign Up">
-              <button 
-                type="button" 
-                className="btn sign Up">
-                sign up
-              </button>
-            </div>
-          {/* </section> */}
+                <div class="form-group" class="col-centered">
+                    <input 
+                        className="formField"
+                        type="text"
+                        placeholder="Email Adress"
+                        name="email" 
+                        value={email}
+                        onChange = {e => onChange(e)}
+                        required/>
+                </div>
+
+                <div class="form-group" class="col-centered" >
+                    <input
+                        className="formField"
+                        type="password"
+                        placeholder="* Password"
+                        name="password"
+                        value={password}
+                        onChange = {e => onChange(e)}
+                        required />
+                </div>
+                
+                <div className="sign In" className="col-centered">
+                    <DribbleButton className="AnimateButton" color="deep-orange" onClick={"TODO"} animationDuration={1000} >
+                      <div className="LogInText">
+                          התחבר
+                        </div>   
+                    </DribbleButton>
+                </div>
+
+                <div class="sign Up" class="col-centered">
+                <DribbleButton className="AnimateButton" color="yellow" onClick={"TODO"} animationDuration={1000} >
+                         הרשם
+                    </DribbleButton>
+                </div>
+
+                
+            </section>
         </body>
-        // </Fragment>
+        </html>
+        </Fragment>
 
     )
 }
-
 export default LoginAdmin
