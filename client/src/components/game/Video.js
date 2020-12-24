a100d9f0d620dff7ed28d16bf02836c51c56f9d0
BarakFalach <barak8995@gmail.com>
2020-12-24 10:51:14 -0800

    add Video phase

--- client/src/components/game/Video.js
+++ client/src/components/game/Video.js
@@ -1,5 +1,26 @@
 import React from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
 
-export const Video = () => {
-  return <div></div>;
+const Video = ({ videoUrl }) => {
+  return (
+    <div>
+      <iframe
+        src={videoUrl}
+        width="640"
+        height="360"
+        frameborder="0"
+        allow="autoplay; fullscreen"
+        allowfullscreen
+      ></iframe>
+    </div>
+  );
 };
+Video.prototype = {
+  videoUrl: PropTypes.string,
+};
+
+const mapStateToProps = (state) => ({
+  videoUrl: state.user.userState.phaseProp.videoUrl,
+});
+export default connect(mapStateToProps, {})(Video);
