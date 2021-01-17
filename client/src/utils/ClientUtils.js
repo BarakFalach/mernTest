8e35ccfb066916f63f3924626c97725b812deb84
BarakFalach <barak8995@gmail.com>
2021-01-17 08:36:01 -0800

    dev

--- client/src/utils/ClientUtils.js
+++ client/src/utils/ClientUtils.js
@@ -2,5 +2,5 @@ export const ServerPORT = 8000;
 console.log("Prod", process.env.NODE_ENV);
 export const PATH =
   process.env.NODE_ENV === "production"
-    ? window.location.origin.replace(/^http/, "wss")
+    ? window.location.origin.replace(/^https/, "wss")
     : "ws://localhost";
