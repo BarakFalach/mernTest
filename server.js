c653c36c6b7c4e43010abe6eb92c317347b50614
BarakFalach <barak8995@gmail.com>
2021-01-17 07:31:13 -0800

    de

--- server.js
+++ server.js
@@ -19,9 +19,9 @@ if (process.env.NODE_ENV === "production") {
   // Set static folder
   app.use(express.static("client/build"));
 
-  // app.get("*", (req, res) => {
-  //   res.sendFile(path.resolve(__dirname, "client", "build", "index.hmtl"));
-  // });
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.hmtl"));
+  });
 }
 
 const PORT = process.env.PORT || 5000;
@@ -72,7 +72,9 @@ const ws_PORT = 8000;
 const INDEX = "/index.html";
 
 const server = express()
-  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+  .use((req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.hmtl"))
+  )
   .listen(ws_PORT, () => console.log(`Listening on ${ws_PORT}`));
 
 const { Server } = require("ws");
