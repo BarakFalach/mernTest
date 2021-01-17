e0c25e9287da81150d8d6e514c82efa9df515305
BarakFalach <barak8995@gmail.com>
2021-01-17 02:45:07 -0800

    d

--- client/src/utils/ClientUtils.js
+++ client/src/utils/ClientUtils.js
@@ -2,5 +2,5 @@ export const ServerPORT = 8000;
 console.log("Prod", process.env.NODE_ENV);
 export const PATH =
   process.env.NODE_ENV === "production"
-    ? "ws://cryptic-earth-72422.herokuapp.com"
+    ? "wss://cryptic-earth-72422.herokuapp.com"
     : "wss://localhost";
