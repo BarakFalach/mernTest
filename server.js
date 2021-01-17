1e62afc4001831b0d6ba355388951f68fb35baaa
BarakFalach <barak8995@gmail.com>
2021-01-17 01:58:07 -0800

    deploy 2

--- server.js
+++ server.js
@@ -13,6 +13,15 @@ app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/admin", require("./routes/api/admin"));
 
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.hmtl"));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log("Server Started at: " + PORT));
