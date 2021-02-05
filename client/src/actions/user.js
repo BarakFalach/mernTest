4ff7440fd17732f896a9ceb53d30be2af731f8d4
BarakFalach <barak8995@gmail.com>
2021-02-05 20:26:10 +0200

    user action, answer-1

--- client/src/actions/user.js
+++ client/src/actions/user.js
@@ -1,61 +1,61 @@
-import { KEYGAME_SUCCESS, KEYGAME_FAIL, CHANGE_SCREEN } from "./types";
-import { w3cwebsocket as W3CWebSocket } from "websocket";
-import { PATH, ServerPORT } from "../utils/ClientUtils";
+import { KEYGAME_SUCCESS, KEYGAME_FAIL, CHANGE_SCREEN } from './types';
+import { w3cwebsocket as W3CWebSocket } from 'websocket';
+import { PATH, ServerPORT } from '../utils/ClientUtils';
 var client;
 //Login User
 export const login = ({ name, keygame }) => async (dispatch) => {
-  try {
-    client = new W3CWebSocket(PATH + ":" + ServerPORT);
+	try {
+		client = new W3CWebSocket(PATH + ':' + ServerPORT);
 
-    client.onopen = () => {
-      client.send(
-        JSON.stringify({
-          type: "REQ_USER_LOGIN",
-          name: name,
-          keygame: keygame,
-        })
-      );
-    };
-    client.onmessage = (message) => {
-      const dataFromServer = JSON.parse(message.data);
-      const type = dataFromServer.type;
-      console.log("got reply! ", dataFromServer);
-      if (dataFromServer) {
-        dispatch({
-          type: type,
-          payload: dataFromServer,
-        });
-      }
-    };
-  } catch (err) {
-    // const errors = err.response.data.errors;
-    // if (errors) {
-    //   errors.forEach((error) => dispatch(setAlert(error.msg, "danger")));
-    // }
-    console.log(err.message);
-    dispatch({
-      type: KEYGAME_FAIL,
-    });
-  }
+		client.onopen = () => {
+			client.send(
+				JSON.stringify({
+					type: 'REQ_USER_LOGIN',
+					name: name,
+					keygame: keygame,
+				})
+			);
+		};
+		client.onmessage = (message) => {
+			const dataFromServer = JSON.parse(message.data);
+			const type = dataFromServer.type;
+			console.log('got reply! ', dataFromServer);
+			if (dataFromServer) {
+				dispatch({
+					type: type,
+					payload: dataFromServer,
+				});
+			}
+		};
+	} catch (err) {
+		// const errors = err.response.data.errors;
+		// if (errors) {
+		//   errors.forEach((error) => dispatch(setAlert(error.msg, "danger")));
+		// }
+		console.log(err.message);
+		dispatch({
+			type: KEYGAME_FAIL,
+		});
+	}
 };
 
 //TODO MIGHT NEED =>async dispatch
 export const UserAnswer = (AnswerNum, time) => async () => {
-  console.log("Answer Num" + AnswerNum);
-  console.log("Time" + time);
-  client.send(
-    JSON.stringify({
-      type: "USER_ANSWER",
-      answer: AnswerNum,
-      time: time,
-    })
-  );
+	console.log('Answer Num' + AnswerNum);
+	console.log('Time' + time);
+	client.send(
+		JSON.stringify({
+			type: 'USER_ANSWER',
+			answer: AnswerNum - 1,
+			time: time,
+		})
+	);
 };
 
 export const videoEnd = () => async () => {
-  client.send(
-    JSON.stringify({
-      type: "VIDEO_END",
-    })
-  );
+	client.send(
+		JSON.stringify({
+			type: 'VIDEO_END',
+		})
+	);
 };
