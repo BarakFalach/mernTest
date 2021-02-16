9feaa3dd9219d1ca91c590b4fe463efc191292c1
BarakFalach <barak8995@gmail.com>
2021-02-16 17:14:06 +0200

    timer, defualt answer, top3,csv

--- serverClasses/gameDefintionDev.js
+++ serverClasses/gameDefintionDev.js
@@ -7,6 +7,7 @@ gameDefenition["video1"] = {
   phaseProp: {
     videoUrl: "https://vimeo.com/204414561",
   },
+  duration: 32,
 };
 gameDefenition["question1"] = {
   key: "question1",
@@ -19,6 +20,17 @@ gameDefenition["question1"] = {
   },
   correct_answer: 1,
   answerAudio: audioUrl,
+  duration: 5,
+};
+gameDefenition["bars1"] = {
+  key: "bars1",
+  type: "Bars",
+  duration: 5,
+};
+gameDefenition["top3_1"] = {
+  key: "top3_1",
+  type: "Top3",
+  duration: 5,
 };
 gameDefenition["question2"] = {
   key: "question2",
@@ -31,6 +43,12 @@ gameDefenition["question2"] = {
   },
   correct_answer: 1,
   answerAudio: audioUrl,
+  duration: 5,
+};
+gameDefenition["bars2"] = {
+  key: "bars2",
+  type: "Bars",
+  duration: 5,
 };
 gameDefenition["video2"] = {
   key: "video2",
@@ -38,6 +56,7 @@ gameDefenition["video2"] = {
   phaseProp: {
     videoUrl: "https://vimeo.com/494218419/fc3e5c2cf6",
   },
+  duration: 33,
 };
 gameDefenition["question3"] = {
   key: "question3",
@@ -50,5 +69,11 @@ gameDefenition["question3"] = {
   },
   correct_answer: 1,
   answerAudio: audioUrl,
+  duration: 5,
+};
+gameDefenition["bars3"] = {
+  key: "bars3",
+  type: "Bars",
+  duration: 5,
 };
 exports.gameDefenition = gameDefenition;
