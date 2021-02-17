2ae1dc059eef20b212d227dafb3e86e31efed17f
asafsalo <3asafi51@gmail.com>
2021-02-17 12:51:42 +0200

    hola 2

--- client/src/components/game/Top3.js
+++ client/src/components/game/Top3.js
@@ -2,10 +2,13 @@ import React, { Fragment } from "react";
 import WinnerShapeSVG from "../../assets/winner_Shape.svg";
 import IconPerson from "../../assets/person.jpg";
 import Crown from "../../assets/crown.svg";
+import audio_example from "../../assets/audio_example.mp3"
 import Spotlight from 'react-spotlight';
 
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
+import ReactAudioPlayer from 'react-audio-player';
+
 
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
@@ -16,14 +19,16 @@ import Confetti from "react-confetti";
 import "../layouts/css/Top3.css";
 
 
-const users = {
-  first: {id: 13, place: 1, score: 754},
-  second: {id: 6, place: 2, score: 654},
-  third: {id: 23, place: 3, score: 651}
-};
+// const users = {
+//   first: {id: 13, place: 1, score: 754},
+//   second: {id: 6, place: 2, score: 654},
+//   third: {id: 23, place: 3, score: 651}
+// };
 
 
-export const Top3 = ({ users2, audio }) => {
+export const Top3 = ({ users, audio }) => {
+
+  console.log(users);
   
   return (
 
@@ -58,7 +63,6 @@ export const Top3 = ({ users2, audio }) => {
         </div>
       </Spotlight>
 
-
     {/* Users */}
     <div className="flex-container-main">
         
@@ -66,11 +70,11 @@ export const Top3 = ({ users2, audio }) => {
         <div className="flex-container-col">
           <div className="empty-rec"/>
           <div className="item-not-flex">
-            <div class="ellipse">{users.third.id}</div>
+            <div class="ellipse">{users[0].user_name}</div>
             <img alt="playerIcon" src={IconPerson} width="145px"/>
           </div>
 
-          <div className="score-text inline-block">{users.third.score}</div> 
+          <div className="score-text inline-block">{users[0].curr_score}</div> 
           <img className="item" alt="icon place 3" src={WinnerShapeSVG} width="130px"/>
         </div> 
 
@@ -79,10 +83,10 @@ export const Top3 = ({ users2, audio }) => {
           <div className="empty-rec"/>
           <img alt="playerIcon" src={Crown} width="90px" style={{transform: "rotate(10deg)"}}/>
           <div className="item-not-flex">
-            <div class="ellipse">{users.first.id}</div>
+            <div class="ellipse">{users[0].user_name}</div>
             <img alt="playerIcon" src={IconPerson} width="145px"/>
           </div>
-          <div className="score-text">{users.first.score}</div> 
+          <div className="score-text">{users[0].curr_score}</div> 
           <img className="item big-item" alt="icon place 1" src={WinnerShapeSVG} width="180px"/>     
           <div className="empty-rec"/>
         </div> 
@@ -91,10 +95,10 @@ export const Top3 = ({ users2, audio }) => {
         <div className="flex-container-col">
           <div className="empty-rec"/>
           <div className="item-not-flex">
-            <div class="ellipse">{users.second.id}</div>
+            <div class="ellipse">{users[0].user_name}</div>
             <img alt="playerIcon" src={IconPerson} width="145px"/>
           </div>
-          <div className="score-text">{users.second.score}</div> 
+          <div className="score-text">{users[0].curr_score}</div> 
           <img className="item" alt="icon place 2" src={WinnerShapeSVG} width="130px"/>
         </div> 
 
