9720d628d923b0200cbdedd49a283f9b443de9ed
asafsalo <3asafi51@gmail.com>
2021-02-20 16:33:21 +0200

    asaf-Top3-Webcam-Login

--- client/src/components/game/Phase.js
+++ client/src/components/game/Phase.js
@@ -8,6 +8,7 @@ import { Answer } from "./Answer";
 import Video from "./Video";
 import Top3 from "./Top3";
 import { Poll } from "./Poll";
+import WebcamCapture from "./WebcamCapture";
 //import EdenTesting from "./EdenTesting";
 
 const Phase = ({ phase }) => {
@@ -25,7 +26,8 @@ const Phase = ({ phase }) => {
     case "bars":
       return <Bars />;
     default:
-    return <h1>Defult Screen</h1>;
+      return <WebcamCapture/>
+    // return <h1>Defult Screen</h1>;
     // return <EdenTesting />;
   }
   return <div></div>;
