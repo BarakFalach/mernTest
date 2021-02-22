47c2c206363d3e1056d9889682eee3ce3ba241c6
berdugo1994 <berdugogo@gmail.com>
2021-02-22 20:40:47 +0200

    friends bar

--- client/src/components/game/Welcome.js
+++ client/src/components/game/Welcome.js
@@ -1,22 +1,51 @@
-import React, { Component } from "react";
+import React from "react";
 import "../layouts/css/Welcome.css";
 import LinearProgress from "@material-ui/core/LinearProgress";
-// import Chart from "./assets/Chart.js";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
 
-export default class Welcome extends React.Component {
-  constructor() {
-    super();
-  }
-
-  render() {
-    const welcome = (
+const Welcome = ({ ratio }) => {
+  console.log("ratio updated and " + ratio);
+  if (ratio < 0) {
+    return "Bad ratio from server";
+  } else if (ratio < 0.8) {
+    return (
       <div className='wholescreen'>
-        <div className='welcome-label'>
-          The game will resume shortley
-          <LinearProgress />
+        <div dir='rtl' style={{ width: "60%" }} className='welcome-label'>
+          ממתין לחברים..
+          <LinearProgress
+            style={{ height: "8%", marginTop: "5%" }}
+            color='primary'
+            variant='determinate'
+            value={ratio * 100}
+          />
         </div>
       </div>
     );
-    return welcome;
+  } else {
+    ratio = ratio >= 1 ? 1 : ratio;
+    return (
+      <div className='wholescreen'>
+        <div dir='rtl' style={{ width: "60%" }} className='welcome-label'>
+          כבר מתחילים!
+          <LinearProgress
+            style={{ height: "8%", marginTop: "5%" }}
+            color='secondary'
+            width='30px'
+            variant='determinate'
+            value={ratio * 100}
+          />
+        </div>
+      </div>
+    );
   }
-}
+};
+
+Welcome.propTypes = {
+  ratio: PropTypes.number.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  ratio: state.user.userState.phaseProp.ratio,
+});
+export default connect(mapStateToProps, {})(Welcome);
