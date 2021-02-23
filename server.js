67f39d51bd1a8a8b9e8216bc71d1f77f9d28d08a
BarakFalach <barak8995@gmail.com>
2021-02-23 13:41:01 +0200

    https

--- server.js
+++ server.js
@@ -1,7 +1,26 @@
-const express = require('express');
-const connectDB = require('./config/db');
-const path = require('path');
+const express = require("express");
+const connectDB = require("./config/db");
+const path = require("path");
+const http = require("http");
+const https = require("https");
+const WebSocket = require("ws");
+const production = process.env.NODE_ENV === "production";
+console.log(process.env.NODE_ENV);
+const ws_PORT = 8000;
+const INDEX = "/index.html";
+const fs = require("fs");
+var sshKey = production
+  ? fs.readFileSync("/etc/ssl/private/apache-selfsigned.key")
+  : "";
+var cert = production
+  ? fs.readFileSync("/etc/ssl/certs/apache-selfsigned.crt")
+  : "";
+var options = {
+  key: sshKey,
+  cert: cert,
+};
 const app = express();
+
 //  Connect Database
 connectDB();
 
@@ -9,63 +28,64 @@ connectDB();
 app.use(express.json({ extended: false }));
 
 // Define Routesnpm
-app.use('/api/users', require('./routes/api/users'));
-app.use('/api/auth', require('./routes/api/auth'));
-app.use('/api/admin', require('./routes/api/admin'));
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/admin", require("./routes/api/admin"));
 
 //Server static assets in production
 
 // Set static folder
-app.use(express.static('client/build'));
+app.use(express.static("client/build"));
 
-app.get('*', (req, res) => {
-	res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 });
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log('Server Started at: ' + PORT));
+app.listen(PORT, () => console.log("Server Started at: " + PORT));
+var server;
+server = production ? https.createServer(options, app) : http.createServer();
+const wsServer = new WebSocket.Server({ server });
+server.listen(ws_PORT);
 
 //#################################################################################################################################################################################################################
 
 // Const enums
 const printLogs = false;
 const [
-	MAKAF,
-	USER_ANSWER,
-	PHASE,
-	REQ_USER_LOGIN,
-	GAME_KEY_SUCCESS,
-	GAME_KEY_FAIL,
-	CREATE_NEW_GAME_INSTANCE,
-	VIDEO_END,
-	PAUSE,
-	RESUME,
-	IMG,
-	CAMERA_NOT_ALLOWED,
+  MAKAF,
+  USER_ANSWER,
+  PHASE,
+  REQ_USER_LOGIN,
+  GAME_KEY_SUCCESS,
+  GAME_KEY_FAIL,
+  CREATE_NEW_GAME_INSTANCE,
+  VIDEO_END,
+  PAUSE,
+  RESUME,
+  IMG,
+  CAMERA_NOT_ALLOWED,
 ] = [
-	40,
-	'USER_ANSWER',
-	'PHASE',
-	'REQ_USER_LOGIN',
-	'GAME_KEY_SUCCESS',
-	'GAME_KEY_FAIL',
-	'CREATE_NEW_GAME_INSTANCE',
-	'VIDEO_END',
-	'PAUSE',
-	'RESUME',
-	'IMG',
-	'CAMERA_NOT_ALLOWED',
+  40,
+  "USER_ANSWER",
+  "PHASE",
+  "REQ_USER_LOGIN",
+  "GAME_KEY_SUCCESS",
+  "GAME_KEY_FAIL",
+  "CREATE_NEW_GAME_INSTANCE",
+  "VIDEO_END",
+  "PAUSE",
+  "RESUME",
+  "IMG",
+  "CAMERA_NOT_ALLOWED",
 ];
-const User = require('./serverClasses/user');
-const Admin = require('./serverClasses/admin');
-const RuningGame = require('./serverClasses/runingGame');
-var gameDefenition = require('./serverClasses/gameDefintionDev');
+const User = require("./serverClasses/user");
+const Admin = require("./serverClasses/admin");
+const RuningGame = require("./serverClasses/runingGame");
+var gameDefenition = require("./serverClasses/gameDefintionDev");
 var gameDefenition = gameDefenition.gameDefenition;
-const https = require('http');
-const WebSocket = require('ws');
-const production = process.env.NODE_ENV === 'production';
-console.log(process.env.NODE_ENV);
+
 /**
  * Data Structure explenation for dictionaries:
  *   @var d_admins = {key: server_given_user_id, value: gameKey: number
@@ -80,26 +100,22 @@ const d_admins = {};
 const d_activeGames = {};
 phaseList = [];
 for (key in gameDefenition) {
-	phaseList.push(key);
+  phaseList.push(key);
 }
-const ws_PORT = 8000;
-const INDEX = '/index.html';
 
-const server = https.createServer();
-const wsServer = new WebSocket.Server({ server });
-
-const { connection } = require('mongoose');
+const { connection } = require("mongoose");
+const { fstat } = require("fs");
 
 /** This function generates unique userId for every user
  *  return: The uniqe userId
  */
 //TODO:: can be moved to User Module
 const getUniqueID = () => {
-	const s4 = () =>
-		Math.floor((1 + Math.random()) * 0x10000)
-			.toString(16)
-			.substring(1);
-	return s4() + s4() + '-' + s4();
+  const s4 = () =>
+    Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  return s4() + s4() + "-" + s4();
 };
 
 /** This function generates unique Game-Key
@@ -109,114 +125,114 @@ const getUniqueID = () => {
  *  return: Array of the sorted users
  */
 const getUniqueGameKey = () => {
-	(max = 9999), (min = 1111);
-	var gameKey;
-	do {
-		gameKey = Math.floor(Math.random() * (max - min)) + min;
-	} while (gameKey.toString() in d_activeGames);
-	if (!production) gameKey = 1;
+  (max = 9999), (min = 1111);
+  var gameKey;
+  do {
+    gameKey = Math.floor(Math.random() * (max - min)) + min;
+  } while (gameKey.toString() in d_activeGames);
+  if (!production) gameKey = 1;
 
-	// setGameProperties(gameKey, admin_ID, gameType, numOfParticipates);
-	return gameKey.toString();
+  // setGameProperties(gameKey, admin_ID, gameType, numOfParticipates);
+  return gameKey.toString();
 };
 
 /** This function print the instance of a new user
  *  return: null
  */
 const log_activated_new_user_instance = (gameKey, userID) => {
-	log_print_structure_head('New User Created (' + userID + ')');
-	for (const [key, value] of Object.entries(
-		d_activeGames[gameKey].d_users[userID]
-	)) {
-		log_print_structure_body(key + ': ' + value);
-	}
-	log_print_structure_end();
+  log_print_structure_head("New User Created (" + userID + ")");
+  for (const [key, value] of Object.entries(
+    d_activeGames[gameKey].d_users[userID]
+  )) {
+    log_print_structure_body(key + ": " + value);
+  }
+  log_print_structure_end();
 };
 
 /**
  *  These 3 function beloew (log_prints) create a structure of an order message
  */
 const log_print_structure_head = (header) => {
-	const newMakaf = '-'.repeat((MAKAF - header.length) / 2);
-	console.log('\n' + newMakaf + header + newMakaf);
+  const newMakaf = "-".repeat((MAKAF - header.length) / 2);
+  console.log("\n" + newMakaf + header + newMakaf);
 };
 
 const log_print_structure_body = (body) => {
-	const spaces = ' '.repeat(MAKAF - (body.length + 4));
-	console.log('# ' + body + spaces + ' #');
+  const spaces = " ".repeat(MAKAF - (body.length + 4));
+  console.log("# " + body + spaces + " #");
 };
 
 const log_print_structure_end = () => {
-	console.log('-'.repeat(MAKAF) + '\n');
+  console.log("-".repeat(MAKAF) + "\n");
 };
 
 /** This function is a recursive function to help "log_game_status" function
  *  return: null
  */
 const log_game_status_sub = (key, value, i) => {
-	if (typeof value === 'object') {
-		log_print_structure_body(' '.repeat(i) + key + ': ');
+  if (typeof value === "object") {
+    log_print_structure_body(" ".repeat(i) + key + ": ");
 
-		for (const [keyS, valueS] of Object.entries(value)) {
-			log_game_status_sub(keyS, valueS, i + 1);
-		}
-	} else {
-		log_print_structure_body(' '.repeat(i) + key + ': ' + value);
-	}
+    for (const [keyS, valueS] of Object.entries(value)) {
+      log_game_status_sub(keyS, valueS, i + 1);
+    }
+  } else {
+    log_print_structure_body(" ".repeat(i) + key + ": " + value);
+  }
 };
 
 /** This function print the game status by given a game key
  *  return: null
  */
 const log_game_status = (gameKey) => {
-	const printName = ' Game Status ' + gameKey + ' ';
-	log_print_structure_head(printName);
-	for (const [key, value] of Object.entries(d_activeGames[gameKey])) {
-		if (key !== 'd_users') {
-			if (typeof value === 'object') {
-				log_game_status_sub(key, value, 0);
-			} else {
-				log_print_structure_body(key + ': ' + value);
-			}
-		}
-	}
-	log_print_structure_end();
+  const printName = " Game Status " + gameKey + " ";
+  log_print_structure_head(printName);
+  for (const [key, value] of Object.entries(d_activeGames[gameKey])) {
+    if (key !== "d_users") {
+      if (typeof value === "object") {
+        log_game_status_sub(key, value, 0);
+      } else {
+        log_print_structure_body(key + ": " + value);
+      }
+    }
+  }
+  log_print_structure_end();
 };
 
 /** This function print the game status by given a game key
  *  return: null
  */
 const log_activeGames = () => {
-	log_print_structure_head(
-		'Current Activated Games (' + Object.keys(d_activeGames).length + ')'
-	);
-	for (var key in d_activeGames) {
-		log_print_structure_body(key);
-	}
-	log_print_structure_end();
+  log_print_structure_head(
+    "Current Activated Games (" + Object.keys(d_activeGames).length + ")"
+  );
+  for (var key in d_activeGames) {
+    log_print_structure_body(key);
+  }
+  log_print_structure_end();
 };
 
 /** This function print that the user insert not active game
  *  return: null
  */
 const log_fail_game_key = (gameKey) => {
-	log_print_structure_head('Game Key Error (' + gameKey + ')');
-	log_print_structure_body('User insert not activate game key');
-	log_print_structure_end();
+  log_print_structure_head("Game Key Error (" + gameKey + ")");
+  log_print_structure_body("User insert not activate game key");
+  log_print_structure_end();
 };
 
 /** This function update the user that there is no such a game key in the server
  *  return: send a fail message
  */
 const handle_bad_req_user_login = (connection, gameKey) => {
-	connection.send(
-		JSON.stringify({
-			type: GAME_KEY_FAIL,
-		})
-	);
+  connection.send(
+    JSON.stringify({
+      type: GAME_KEY_FAIL,
+    })
+  );
 
-	// TODO: remove these prints
-	if (printLogs) log_fail_game_key(gameKey);
+  // TODO: remove these prints
+  if (printLogs) log_fail_game_key(gameKey);
 };
 
 /** This function create a new game key
@@ -224,56 +240,56 @@ const handle_bad_req_user_login = (connection, gameKey) => {
  *  TODO: handle in admin - present the game key in the admin dashboard
  */
 const handle_new_game_instance = (
-	admin_ID,
-	connection,
-	numOfParticipates = 1,
-	name,
-	gameType = 1
+  admin_ID,
+  connection,
+  numOfParticipates = 1,
+  name,
+  gameType = 1
 ) => {
-	var new_admin;
-	if (name in d_admins) {
-		new_admin = d_admins[name];
-		new_admin.setConnection(connection);
-		d_activeGames[new_admin.gameKey].admin = new_admin;
-		connection.send(
-			JSON.stringify({
-				type: CREATE_NEW_GAME_INSTANCE,
-				keyGame: new_admin.gameKey,
-				phaseList: phaseList,
-				numOfPlayers: numOfParticipates,
-			})
-		);
-		d_activeGames[new_admin.gameKey].admin_re_enter();
-		return new_admin.gameKey;
-	}
-	const gameKey = getUniqueGameKey(admin_ID, gameType, numOfParticipates);
+  var new_admin;
+  if (name in d_admins) {
+    new_admin = d_admins[name];
+    new_admin.setConnection(connection);
+    d_activeGames[new_admin.gameKey].admin = new_admin;
+    connection.send(
+      JSON.stringify({
+        type: CREATE_NEW_GAME_INSTANCE,
+        keyGame: new_admin.gameKey,
+        phaseList: phaseList,
+        numOfPlayers: numOfParticipates,
+      })
+    );
+    d_activeGames[new_admin.gameKey].admin_re_enter();
+    return new_admin.gameKey;
+  }
+  const gameKey = getUniqueGameKey(admin_ID, gameType, numOfParticipates);
 
-	new_admin = new Admin(admin_ID, gameKey);
-	d_admins[name] = new_admin;
+  new_admin = new Admin(admin_ID, gameKey);
+  d_admins[name] = new_admin;
 
-	d_activeGames[gameKey] = new RuningGame(
-		new_admin,
-		gameType,
-		numOfParticipates,
-		phaseList,
-		gameDefenition
-	);
-	if (printLogs) {
-		log_activeGames();
-		log_game_status(gameKey);
-	}
-	d_admins[name].setConnection(connection);
-	// d_connections[userID] = connection;
+  d_activeGames[gameKey] = new RuningGame(
+    new_admin,
+    gameType,
+    numOfParticipates,
+    phaseList,
+    gameDefenition
+  );
+  if (printLogs) {
+    log_activeGames();
+    log_game_status(gameKey);
+  }
+  d_admins[name].setConnection(connection);
+  // d_connections[userID] = connection;
 
-	connection.send(
-		JSON.stringify({
-			type: CREATE_NEW_GAME_INSTANCE,
-			keyGame: gameKey,
-			phaseList: phaseList,
-			numOfPlayers: numOfParticipates,
-		})
-	);
-	return gameKey;
+  connection.send(
+    JSON.stringify({
+      type: CREATE_NEW_GAME_INSTANCE,
+      keyGame: gameKey,
+      phaseList: phaseList,
+      numOfPlayers: numOfParticipates,
+    })
+  );
+  return gameKey;
 };
 
 /** This function delete the admin from server (and his connections)
@@ -281,104 +297,102 @@ const handle_new_game_instance = (
  *  return: null
  */
 const handle_delete_admin = (userID, gameKey) => {
-	// delete_game_instance(gameKey);
-	// delete d_connections[userID];
-	console.log('SERVER : Admin conenction closed (userID: ' + userID + ')');
+  // delete_game_instance(gameKey);
+  // delete d_connections[userID];
+  console.log("SERVER : Admin conenction closed (userID: " + userID + ")");
 };
 
 /** This function delete the game instance from the server
  *
  */
 const delete_game_instance = (gameKey) => {
-	for (let user in d_activeGames[gameKey].d_users)
-		d_activeGames[gameKey].handle_delete_user(user, gameKey);
-	delete d_activeGames[gameKey];
-	log_activeGames();
+  for (let user in d_activeGames[gameKey].d_users)
+    d_activeGames[gameKey].handle_delete_user(user, gameKey);
+  delete d_activeGames[gameKey];
+  log_activeGames();
 };
 
-wsServer.on('connection', (request) => {
-	try {
-		log_print_structure_head('Server: Connection Request');
-		const connection = request;
-		var gameKey;
-		var userName;
-		// const userID = production ? request._socket.remoteAddress : getUniqueID();
-		const userID = getUniqueID();
+wsServer.on("connection", (request) => {
+  try {
+    log_print_structure_head("Server: Connection Request");
+    const connection = request;
+    var gameKey;
+    var userName;
+    // const userID = production ? request._socket.remoteAddress : getUniqueID();
+    const userID = getUniqueID();
 
-		connection.on('message', function (message) {
-			try {
-				const userlog = JSON.parse(message);
+    connection.on("message", function (message) {
+      try {
+        const userlog = JSON.parse(message);
 
-				// first message
-				if (undefined === (userName || gameKey)) {
-					userName = userlog.name;
-					gameKey = userlog.keygame;
-				}
+        // first message
+        if (undefined === (userName || gameKey)) {
+          userName = userlog.name;
+          gameKey = userlog.keygame;
+        }
 
-				switch (userlog.type) {
-					case CREATE_NEW_GAME_INSTANCE:
-						gameKey = handle_new_game_instance(
-							userID,
-							connection,
-							userlog.numOfPlayers,
-							userlog.name
-						);
-						break;
+        switch (userlog.type) {
+          case CREATE_NEW_GAME_INSTANCE:
+            gameKey = handle_new_game_instance(
+              userID,
+              connection,
+              userlog.numOfPlayers,
+              userlog.name
+            );
+            break;
 
-					case REQ_USER_LOGIN:
-						if (gameKey in d_activeGames) {
-							d_activeGames[gameKey].handle_req_user_login(
-								userID,
-								userName,
-								connection,
-								gameKey
-							);
-						} else handle_bad_req_user_login(connection, gameKey);
-						break;
+          case REQ_USER_LOGIN:
+            if (gameKey in d_activeGames) {
+              d_activeGames[gameKey].handle_req_user_login(
+                userID,
+                userName,
+                connection,
+                gameKey
+              );
+            } else handle_bad_req_user_login(connection, gameKey);
+            break;
 
-					case PHASE:
-						d_activeGames[gameKey].handle_change_screen(userlog.phaseName);
-						break;
+          case PHASE:
+            d_activeGames[gameKey].handle_change_screen(userlog.phaseName);
+            break;
 
-					case USER_ANSWER:
-						d_activeGames[gameKey].handle_user_answer(
-							userID,
-							userlog.answer,
-							userlog.time,
-							userlog.key
-						);
-						break;
-					case VIDEO_END:
-						d_activeGames[gameKey].handler_user_video_end();
-						// handle_user_answer(gameKey, userID, userlog.answer, userlog.time);
-						break;
-					case PAUSE:
-						d_activeGames[gameKey].setPause();
-						break;
-					case RESUME:
-						d_activeGames[gameKey].setResume();
-						d_activeGames[gameKey].handle_change_screen();
-						break;
-					case IMG:
-						d_activeGames[gameKey].handle_user_img(userID, userlog.img);
-					case CAMERA_NOT_ALLOWED:
-						d_activeGames[gameKey].handle_user_img(userID, '');
-				}
-			} catch (err) {
-				console.log(err.message);
-			}
-		});
+          case USER_ANSWER:
+            d_activeGames[gameKey].handle_user_answer(
+              userID,
+              userlog.answer,
+              userlog.time,
+              userlog.key
+            );
+            break;
+          case VIDEO_END:
+            d_activeGames[gameKey].handler_user_video_end();
+            // handle_user_answer(gameKey, userID, userlog.answer, userlog.time);
+            break;
+          case PAUSE:
+            d_activeGames[gameKey].setPause();
+            break;
+          case RESUME:
+            d_activeGames[gameKey].setResume();
+            d_activeGames[gameKey].handle_change_screen();
+            break;
+          case IMG:
+            d_activeGames[gameKey].handle_user_img(userID, userlog.img);
+          case CAMERA_NOT_ALLOWED:
+            d_activeGames[gameKey].handle_user_img(userID, "");
+        }
+      } catch (err) {
+        console.log(err.message);
+      }
+    });
 
-		connection.on('close', function (connection) {
-			if (gameKey in d_activeGames)
-				if (userID in d_activeGames[gameKey].d_users)
-					d_activeGames[gameKey].handle_delete_user(userID, gameKey);
+    connection.on("close", function (connection) {
+      if (gameKey in d_activeGames)
+        if (userID in d_activeGames[gameKey].d_users)
+          d_activeGames[gameKey].handle_delete_user(userID, gameKey);
 
-			if (userID in d_admins) handle_delete_admin(userID, d_admins[userID]);
-		});
-	} catch (err) {
-		console.log(err.message);
-	}
+      if (userID in d_admins) handle_delete_admin(userID, d_admins[userID]);
+    });
+  } catch (err) {
+    console.log(err.message);
+  }
 });
-
-server.listen(ws_PORT);
--- serverClasses/gameDefintionDev.js
+++ serverClasses/gameDefintionDev.js
@@ -1,111 +1,111 @@
 const gameDefenition = {};
-const audioUrl = 'https://www.dropbox.com/s/sggi2zxzwfnur5w/sasfa.wav?raw=1';
+const audioUrl = "https://www.dropbox.com/s/sggi2zxzwfnur5w/sasfa.wav?raw=1";
 
-gameDefenition['video1'] = {
-	key: 'video1',
-	type: 'video',
-	phaseProp: {
-		videoUrl: 'https://vimeo.com/204414561',
-	},
-	duration: 32,
+gameDefenition["video1"] = {
+  key: "video1",
+  type: "video",
+  phaseProp: {
+    videoUrl: "https://vimeo.com/204414561",
+  },
+  duration: 32,
 };
-gameDefenition['question1'] = {
-	key: 'question1',
-	type: 'Question',
-	phaseProp: {
-		question: 'מה גילכם?',
-		answers: ['חטיבת ביניים', 'חטיבה עליונה', 'אחר'],
-		time: 6,
-		key: 'question1',
-	},
-	correct_answer: 1,
-	duration: 18,
+gameDefenition["question1"] = {
+  key: "question1",
+  type: "Question",
+  phaseProp: {
+    question: "מה גילכם?",
+    answers: ["חטיבת ביניים", "חטיבה עליונה", "אחר"],
+    time: 6,
+    key: "question1",
+  },
+  correct_answer: 1,
+  duration: 18,
 };
-gameDefenition['bars1'] = {
-	key: 'bars1',
-	type: 'Bars',
-	duration: 5,
+gameDefenition["bars1"] = {
+  key: "bars1",
+  type: "Bars",
+  duration: 5,
 };
-gameDefenition['top3_1'] = {
-	key: 'top3_1',
-	type: 'Top3',
-	duration: 20,
-	phaseProp: {
-		audioArr: [
-			'https://www.dropbox.com/s/sggi2zxzwfnur5w/sasfa.wav?raw=1',
-			'https://www.dropbox.com/s/sggi2zxzwfnur5w/sasfa.wav?raw=1',
-			'https://www.dropbox.com/s/sggi2zxzwfnur5w/sasfa.wav?raw=1',
-		],
-	},
+gameDefenition["top3_1"] = {
+  key: "top3_1",
+  type: "Top3",
+  duration: 20,
+  phaseProp: {
+    audioArr: [
+      "https://www.dropbox.com/s/sggi2zxzwfnur5w/sasfa.wav?raw=1",
+      "https://www.dropbox.com/s/sggi2zxzwfnur5w/sasfa.wav?raw=1",
+      "https://www.dropbox.com/s/sggi2zxzwfnur5w/sasfa.wav?raw=1",
+    ],
+  },
 };
-gameDefenition['groups1'] = {
-	key: 'groups1',
-	type: 'Groups',
-	duration: 20,
+gameDefenition["groups1"] = {
+  key: "groups1",
+  type: "Groups",
+  duration: 5,
 };
-gameDefenition['question2'] = {
-	key: 'question2',
+gameDefenition["question2"] = {
+  key: "question2",
 
-	type: 'Question',
-	phaseProp: {
-		question: 'האם אי פעם השתמשתם במריחואנה או גראס?',
-		answers: ['מעולם לא', 'פעם בודדת', 'פעמים בודדות (3-5)'],
-		time: 6,
-		key: 'question2',
-	},
-	correct_answer: 1,
-	duration: 15,
+  type: "Question",
+  phaseProp: {
+    question: "האם אי פעם השתמשתם במריחואנה או גראס?",
+    answers: ["מעולם לא", "פעם בודדת", "פעמים בודדות (3-5)"],
+    time: 6,
+    key: "question2",
+  },
+  correct_answer: 1,
+  duration: 15,
 };
-gameDefenition['bars2'] = {
-	key: 'bars2',
-	type: 'Bars',
-	duration: 5,
+gameDefenition["bars2"] = {
+  key: "bars2",
+  type: "Bars",
+  duration: 5,
 };
-gameDefenition['video2'] = {
-	key: 'video2',
-	type: 'video',
-	phaseProp: {
-		videoUrl: 'https://vimeo.com/494218419/fc3e5c2cf6',
-	},
-	duration: 33,
+gameDefenition["video2"] = {
+  key: "video2",
+  type: "video",
+  phaseProp: {
+    videoUrl: "https://vimeo.com/494218419/fc3e5c2cf6",
+  },
+  duration: 33,
 };
-gameDefenition['question3'] = {
-	key: 'question3',
-	type: 'Question',
-	phaseProp: {
-		question: 'במעבדות הסמים הלא חוקיות',
-		answers: [
-			'מזקקים את הסמים בצורה סטירילית',
-			'מערבבים את חומרי הסם עם רעלים בכדי לחזק את השפעתן',
-			'משתמשים באמצעים חדשניים להפקת הסם',
-			'כל התשובות נכונות',
-		],
-		time: 6,
-		key: 'question3',
-	},
-	correct_answer: 1,
-	duration: 15,
+gameDefenition["question3"] = {
+  key: "question3",
+  type: "Question",
+  phaseProp: {
+    question: "במעבדות הסמים הלא חוקיות",
+    answers: [
+      "מזקקים את הסמים בצורה סטירילית",
+      "מערבבים את חומרי הסם עם רעלים בכדי לחזק את השפעתן",
+      "משתמשים באמצעים חדשניים להפקת הסם",
+      "כל התשובות נכונות",
+    ],
+    time: 6,
+    key: "question3",
+  },
+  correct_answer: 1,
+  duration: 15,
 };
-gameDefenition['bars3'] = {
-	key: 'bars3',
-	type: 'Bars',
-	duration: 5,
+gameDefenition["bars3"] = {
+  key: "bars3",
+  type: "Bars",
+  duration: 5,
 };
-gameDefenition['question4'] = {
-	key: 'question4',
-	type: 'Question',
-	phaseProp: {
-		question: 'במעבדות הסמים הלא חוקיות',
-		answers: ['נכון', 'לא נכון'],
-		time: 6,
-		key: 'question4',
-	},
-	correct_answer: 1,
-	duration: 15,
+gameDefenition["question4"] = {
+  key: "question4",
+  type: "Question",
+  phaseProp: {
+    question: "במעבדות הסמים הלא חוקיות",
+    answers: ["נכון", "לא נכון"],
+    time: 6,
+    key: "question4",
+  },
+  correct_answer: 1,
+  duration: 15,
 };
-gameDefenition['bars4'] = {
-	key: 'bars3',
-	type: 'Bars',
-	duration: 5,
+gameDefenition["bars4"] = {
+  key: "bars3",
+  type: "Bars",
+  duration: 5,
 };
 exports.gameDefenition = gameDefenition;
