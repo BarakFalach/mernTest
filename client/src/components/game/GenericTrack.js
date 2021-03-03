000f705cd51e676e7ea11b8dbea5dcf332cfc7ee
berdugo1994 <berdugogo@gmail.com>
2021-03-03 17:07:00 +0200

    changes

--- client/src/components/game/GenericTrack.js
+++ client/src/components/game/GenericTrack.js
@@ -2,7 +2,7 @@ import React from "react";
 import "../layouts/css/GenericTrack.css";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
-const GenericTrack = ({ mediaKey, paragraph }) => {
+const GenericTrack = ({ mediaKey }) => {
   let audioPath = "assets/generic/" + mediaKey + ".wav";
   let imagePath = "assets/generic/" + mediaKey + ".png";
   let audio = new Audio(audioPath);
@@ -10,29 +10,22 @@ const GenericTrack = ({ mediaKey, paragraph }) => {
   React.useEffect(() => {
     return () => {
       if (!audio.paused || !audio.currentTime) {
+        console.log("generic stop");
         audio.pause();
       }
     };
   }, []);
   return (
     <div className='generic-flexbox'>
-      <img
-        src={imagePath}
-        style={{ fontSize: "10px" }}
-        className='generic-image '
-        alt='not found'
-      />
-      <div className='generic-paragraph'>{paragraph}</div>
+      <img src={imagePath} alt='not found' />
     </div>
   );
 };
 GenericTrack.propTypes = {
   mediaKey: PropTypes.string.isRequired,
-  paragraph: PropTypes.string.isRequired,
 };
 
 const mapStateToProps = (state) => ({
   mediaKey: state.user.userState.phaseProp.key,
-  paragraph: state.user.userState.phaseProp.paragraph,
 });
 export default connect(mapStateToProps, {})(GenericTrack);
